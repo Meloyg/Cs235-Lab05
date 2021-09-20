fcad969b93e62ce696861b8e1ee250865d3e4991
Nicholas Berg <nicholas.bai.berg@gmail.com>
2021-09-20 12:20:32 +1200

    Update tests for Q3

--- q3.py
+++ q3.py
@@ -48,9 +48,10 @@ def test_case1():
     column_name_for_ordering = "ArtistId"
     query_result = get_best_of_albums(database_filename, column_name_for_ordering)
 
-    print("(album id, album title, artist id):")
-    for database_record in query_result:
-        print(database_record)
+    assert query_result[1] == (20, 'The Best Of Buddy Guy - The Millenium Collection', 15)
+    assert query_result[3] == (36, 'Greatest Hits II', 51)
+    assert query_result[5] == (61, "Knocking at Your Back Door: The Best Of Deep Purple in the 80's", 58)
+    assert query_result[7] == (83, 'My Way: The Best Of Frank Sinatra [Disc 1]', 85)
 
 
 def test_case2():
@@ -58,24 +59,24 @@ def test_case2():
     column_name_for_ordering = "AlbumId"
     query_result = get_best_of_albums(database_filename, column_name_for_ordering)
 
-    print("(album id, album title, artist id):")
-    for database_record in query_result:
-        print(database_record)
+    assert query_result[2] == (36, 'Greatest Hits II', 51)
+    assert query_result[4] == (61, "Knocking at Your Back Door: The Best Of Deep Purple in the 80's", 58)
+    assert query_result[6] == (83, 'My Way: The Best Of Frank Sinatra [Disc 1]', 85)
+    assert query_result[8] == (147, 'The Best Of Men At Work', 105)
 
-
-def test_case3():
+def test_case3(capfd):
     database_filename = 'chinook.db'
     column_name_for_ordering = "Id"
     query_result = get_best_of_albums(database_filename, column_name_for_ordering)
-
-    for database_record in query_result:
-        print(database_record)
-
+    out, err = capfd.readouterr()
+    assert out == "ERROR: could not find column 'Id' in the list of columns for table 'albums'!\n"
+    assert query_result == []
 
 def test_case4():
     database_filename = 'chinook.db'
     column_name_for_ordering = "Title"
     query_result = get_best_of_albums(database_filename, column_name_for_ordering)
 
-    for database_record in query_result:
-        print(database_record)
+    assert query_result[1] == (141, 'Greatest Hits', 100)
+    assert query_result[2] == (185, 'Greatest Hits I', 51)
+    assert query_result[3] == (36, 'Greatest Hits II', 51)
