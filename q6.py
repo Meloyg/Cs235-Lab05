a4abe3ce5073294e7b599456afea3c383b4f2d11
Nicholas Berg <nicholas.bai.berg@gmail.com>
2021-09-20 12:39:34 +1200

    update tests for q6

--- q6.py
+++ q6.py
@@ -55,10 +55,14 @@ def test_case1():
     print(f"Playlist '{which_playlist}'")
     for count, database_record in enumerate(query_result):
         print(f"{count:<2} Track Name: {database_record[0]}")
+        assert database_record[0] == "Now's The Time"
         print(f"   Album Title: {database_record[1]}")
+        assert database_record[1] == "The Essential Miles Davis [Disc 1]"
         print(f"   Genre: {database_record[2]}")
+        assert database_record[2] == "Jazz"
         print(f"   Artist: {database_record[3]}")
-        print(f"   Composer: {database_record[4]}")
+        assert database_record[3] == "Miles Davis"
+        print(f"   Composer: {database_record[3]}")
 
 
 def test_case2():
@@ -67,6 +71,8 @@ def test_case2():
     query_result = get_playlist(database_filename, which_playlist)
 
     print(f"Playlist '{which_playlist}'")
+    assert len(query_result) == 26
+    assert (query_result[3]) == ('Restless and Wild', 'Restless and Wild', 'Rock', 'Accept', 'F. Baltes, R.A. Smith-Diesel, S. Kaufman, U. Dirkscneider & W. Hoffman')
     for count, database_record in enumerate(query_result):
         print(f"{count:<2} Track Name: {database_record[0]}")
         print(f"   Album Title: {database_record[1]}")
@@ -75,19 +81,21 @@ def test_case2():
         print(f"   Composer: {database_record[4]}")
 
 
-def test_case3():
+def test_case3(capfd):
+
     database_filename = 'chinook.db'
     which_playlist = 'Independent'
     query_result = get_playlist(database_filename, which_playlist)
-
-    for database_record in query_result:
-        print(database_record)
+    out, err = capfd.readouterr()
+    assert out == "ERROR: Could not find playlist 'Independent' in database!\n"
+    assert query_result == []
 
 
 def test_case4():
     database_filename = 'chinook.db'
     which_playlist = 'Grunge'
     query_result = get_playlist(database_filename, which_playlist)
-
+    assert len(query_result) == 15
+    assert query_result[6] == ('On A Plain', 'Nevermind', 'Rock', 'Nirvana', 'Kurt Cobain')
     for database_record in query_result:
         print(database_record)
