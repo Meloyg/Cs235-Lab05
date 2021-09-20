5cbe6f4574b3ad2d0f5323e7924c8bb3124a841c
Nicholas Berg <nicholas.bai.berg@gmail.com>
2021-09-20 12:27:23 +1200

    Update tests for q5

--- q5.py
+++ q5.py
@@ -40,26 +40,22 @@ def test_case1():
     database_filename = 'chinook.db'
     n = 4
     query_result = get_all_artists_with_at_least_n_albums(database_filename, n)
-
+    assert len(query_result) ==12
     print("(artist id, artist name, number of albums):")
-    for database_record in query_result:
-        print(database_record)
+    assert query_result[11] == (21, 'Various Artists', 4)
 
 
 def test_case2():
     database_filename = 'chinook.db'
     n = 8
     query_result = get_all_artists_with_at_least_n_albums(database_filename, n)
-
-    print("(artist id, artist name, number of albums):")
-    for database_record in query_result:
-        print(database_record)
+    assert len(query_result) == 5
+    assert query_result[4] == (150, 'U2', 10)
 
         
 def test_case3():
     database_filename = 'chinook.db'
     n = 2
     query_result = get_all_artists_with_at_least_n_albums(database_filename, n)
-
-    for database_record in query_result:
-        print(database_record)
+    assert len(query_result) == 56
+    assert query_result[55] == (146, 'Titas', 2)
